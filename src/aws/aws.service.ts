6913ec205a20fa1e19718deacea7b2d4bee76464
Paulo Serrano <pvsserrano@gmail.com>
2024-06-21 15:06:47 -0300

    aws client refactor

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -4,13 +4,7 @@ import { TextractClient, AnalyzeDocumentCommand } from "@aws-sdk/client-textract
 @Injectable()
 export class AwsService {
   async imageToText(file: Express.Multer.File) {
-    const client = new TextractClient({ 
-      region: "us-east-2",
-      credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-      }
-    })
+    const client = new TextractClient({})
     
     try {
       const res = await client.send(new AnalyzeDocumentCommand({ 
