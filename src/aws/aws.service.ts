3fb5276891156ded3821b705f08f2cc6386add77
Paulo Serrano <pvsserrano@gmail.com>
2024-06-21 15:12:31 -0300

    aws debugging

--- src/aws/aws.service.ts
+++ src/aws/aws.service.ts
@@ -4,6 +4,15 @@ import { TextractClient, AnalyzeDocumentCommand } from "@aws-sdk/client-textract
 @Injectable()
 export class AwsService {
   async imageToText(file: Express.Multer.File) {
+    const accessKeyId = process.env.AWS_ACCESS_KEY_ID;
+    const secretAccessKey = process.env.AWS_SECRET_ACCESS_KEY;
+    console.log('AWS_ACCESS_KEY_ID:', accessKeyId)
+    console.log('AWS_SECRET_ACCESS_KEY:', secretAccessKey)
+    
+    if (!accessKeyId || !secretAccessKey) {
+      throw new Error('AWS credentials are not set');
+    }
+
     const client = new TextractClient({})
     
     try {
